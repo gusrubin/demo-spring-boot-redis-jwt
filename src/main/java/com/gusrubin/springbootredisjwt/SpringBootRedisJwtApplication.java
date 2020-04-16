640f70c4e61e2609882240a1539ab0575fde48ec
Gustavo Rubin <gusrubin@gmail.com>
2020-04-15 21:30:13 -0300

    Added project

---
+++ src/main/java/com/gusrubin/springbootredisjwt/SpringBootRedisJwtApplication.java
@@ -0,0 +1,13 @@
+package com.gusrubin.springbootredisjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRedisJwtApplication.class, args);
+	}
+
+}
